ae3f46dfa65e345927a7c263c0ef138f830fa095
Mate Farkas <mate.farkas@sch.hu>
2021-11-19 20:27:58 +0000

    Run RuboCop

--- ruby_lambda/lambda_function.rb
+++ ruby_lambda/lambda_function.rb
@@ -1,20 +1,21 @@
+# frozen_string_literal: true
+
 require 'logger'
 require 'json'
 require 'aws-sdk-lambda'
 require 'aws-sdk-s3'
 
-params = {}
-if ENV['LOCALSTACK_HOSTNAME'] && ENV['LOCALSTACK_PORT']
-  params = { endpoint: "http://#{ENV['LOCALSTACK_HOSTNAME']}:#{ENV['LOCALSTACK_PORT']}" }
+def client_params
+  return {} unless ENV['LOCALSTACK_HOSTNAME'] && ENV['LOCALSTACK_PORT']
+
+  { endpoint: "http://#{ENV['LOCALSTACK_HOSTNAME']}:#{ENV['LOCALSTACK_PORT']}" }
 end
 
-$s3 = Aws::S3::Client.new(params)
-$logger = Logger.new($stdout)
+def lambda_handler(*)
+  logger = Logger.new($stdout)
+  s3 = Aws::S3::Client.new(client_params)
 
-def lambda_handler(event:, context:)
-  $logger.info("BUCKETS: #{$s3.list_buckets.buckets.collect(&:name).to_json}")
+  logger.info("BUCKETS: #{s3.list_buckets.buckets.collect(&:name).to_json}")
 end
 
-if __FILE__ == $PROGRAM_NAME
-  lambda_handler(event: nil, context: nil)
-end
+lambda_handler if __FILE__ == $PROGRAM_NAME
