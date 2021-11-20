d46c2eefd9c728ea9854aa9ef3fbec7ac2569df2
Mate Farkas <mate.farkas@sch.hu>
2021-11-20 18:07:37 +0000

    Use availability zone letters only

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,8 +5,10 @@ locals {
   subnet_cidrs       = cidrsubnets(var.cidr_block, 2, 2, 2, 4, 4, 4)
   availability_zones = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  private_cidrs = zipmap(local.availability_zones, slice(local.subnet_cidrs, 0, 3))
-  public_cidrs  = zipmap(local.availability_zones, slice(local.subnet_cidrs, 3, 6))
+  availability_zone_letters = [ for az in local.availability_zones : substr(az, -1, 1) ]
+
+  private_cidrs = zipmap(local.availability_zone_letters, slice(local.subnet_cidrs, 0, 3))
+  public_cidrs  = zipmap(local.availability_zone_letters, slice(local.subnet_cidrs, 3, 6))
 }
 
 data "aws_availability_zones" "available" {
@@ -21,7 +23,7 @@ resource "aws_subnet" "private" {
   for_each = local.private_cidrs
 
   vpc_id            = aws_vpc.main.id
-  availability_zone = each.key
+  availability_zone = "${data.aws_availability_zones.available.id}${each.key}"
   cidr_block        = each.value
 }
 
@@ -29,7 +31,7 @@ resource "aws_subnet" "public" {
   for_each = local.public_cidrs
 
   vpc_id            = aws_vpc.main.id
-  availability_zone = each.key
+  availability_zone = "${data.aws_availability_zones.available.id}${each.key}"
   cidr_block        = each.value
 }
 
@@ -45,7 +47,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[local.availability_zones[0]].id
+  subnet_id     = aws_subnet.public[local.availability_zone_letters[0]].id
 }
 
 resource "aws_route_table" "private" {
@@ -67,14 +69,14 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "private" {
-  for_each = toset(local.availability_zones)
+  for_each = toset(local.availability_zone_letters)
 
   subnet_id      = aws_subnet.private[each.value].id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "public" {
-  for_each = toset(local.availability_zones)
+  for_each = toset(local.availability_zone_letters)
 
   subnet_id      = aws_subnet.public[each.value].id
   route_table_id = aws_route_table.public.id
--- modules/vpc/migrations.tf
+++ modules/vpc/migrations.tf
@@ -1,60 +1,60 @@
 # NOTE: The migration must use static values
 moved {
   from = aws_subnet.private_a
-  to   = aws_subnet.private["eu-west-1a"]
+  to   = aws_subnet.private["a"]
 }
 
 moved {
   from = aws_subnet.private_b
-  to   = aws_subnet.private["eu-west-1b"]
+  to   = aws_subnet.private["b"]
 }
 
 moved {
   from = aws_subnet.private_c
-  to   = aws_subnet.private["eu-west-1c"]
+  to   = aws_subnet.private["c"]
 }
 
 moved {
   from = aws_subnet.public_a
-  to   = aws_subnet.public["eu-west-1a"]
+  to   = aws_subnet.public["a"]
 }
 
 moved {
   from = aws_subnet.public_b
-  to   = aws_subnet.public["eu-west-1b"]
+  to   = aws_subnet.public["b"]
 }
 
 moved {
   from = aws_subnet.public_c
-  to   = aws_subnet.public["eu-west-1c"]
+  to   = aws_subnet.public["c"]
 }
 
 moved {
   from = aws_route_table_association.private_a
-  to   = aws_route_table_association.private["eu-west-1a"]
+  to   = aws_route_table_association.private["a"]
 }
 
 moved {
   from = aws_route_table_association.private_b
-  to   = aws_route_table_association.private["eu-west-1b"]
+  to   = aws_route_table_association.private["b"]
 }
 
 moved {
   from = aws_route_table_association.private_c
-  to   = aws_route_table_association.private["eu-west-1c"]
+  to   = aws_route_table_association.private["c"]
 }
 
 moved {
   from = aws_route_table_association.public_a
-  to   = aws_route_table_association.public["eu-west-1a"]
+  to   = aws_route_table_association.public["a"]
 }
 
 moved {
   from = aws_route_table_association.public_b
-  to   = aws_route_table_association.public["eu-west-1b"]
+  to   = aws_route_table_association.public["b"]
 }
 
 moved {
   from = aws_route_table_association.public_c
-  to   = aws_route_table_association.public["eu-west-1c"]
+  to   = aws_route_table_association.public["c"]
 }
